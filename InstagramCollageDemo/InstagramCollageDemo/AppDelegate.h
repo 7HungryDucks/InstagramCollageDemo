785fefd83b6bffa943233ceae4c070d3b11dc10d
Yuriy Kholod <yuriy.kholod@e-legion.com>
2014-11-18 14:11:18 +0300

    project added

---
+++ InstagramCollageDemo/InstagramCollageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstagramCollageDemo
+//
+//  Created by Yuri Kholod on 18.11.14.
+//  Copyright (c) 2014 Yuri Kholod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
